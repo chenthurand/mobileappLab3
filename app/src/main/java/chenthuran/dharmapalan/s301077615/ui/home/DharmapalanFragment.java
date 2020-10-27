275af316cdffeb85d156483ba107ed929409f493
chenthurand <71531471+chenthurand@users.noreply.github.com>
2020-10-27 17:45:26 -0400

    second push

--- app/src/main/java/chenthuran/dharmapalan/s301077615/ui/home/DharmapalanFragment.java
+++ app/src/main/java/chenthuran/dharmapalan/s301077615/ui/home/DharmapalanFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
-
+import android.graphics.Color;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import chenthuran.dharmapalan.s301077615.R;
@@ -17,17 +20,42 @@ import chenthuran.dharmapalan.s301077615.R;
 public class DharmapalanFragment extends Fragment {
 
     private DharmapalanViewModel DharmapalanViewModel;
+    private DharmapalanCanvasView Canvas;
+    Color selectedColor;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         DharmapalanViewModel =
                 ViewModelProviders.of(this).get(DharmapalanViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
+        Canvas = (DharmapalanCanvasView) root.findViewById(R.id.canvas);
+        Button buttonSelection = root.findViewById(R.id.pen_option);
+        buttonSelection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Spinner colourSpinner = root.findViewById(R.id.chenthuran_pen_color);
+                TextView colourSpinnerText = (TextView) colourSpinner.getSelectedView();
+                Spinner sizeSpinner = root.findViewById(R.id.chenthuran_pen_size);
+                TextView sizeSpinnerText = (TextView) sizeSpinner.getSelectedView();
+                String color = colourSpinnerText.getText().toString();
+                String thickness = sizeSpinnerText.getText().toString();
+                Canvas.getSelectedOptions(thickness, color);
+            }
+        });
+
+        Button clearOptions = root.findViewById(R.id.clear_option);
+        clearOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Canvas.clearPath();
+
+            }
+        });
+
         DharmapalanViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
             }
         });
         return root;
--- app/src/main/java/chenthuran/dharmapalan/s301077615/ui/home/DharmapalanViewModel.java
+++ app/src/main/java/chenthuran/dharmapalan/s301077615/ui/home/DharmapalanViewModel.java
@@ -1,19 +1,30 @@
 package chenthuran.dharmapalan.s301077615.ui.home;
 //Chenthuran Dharmapalan, Student Number: 301077615, Section: 002
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
 
 public class DharmapalanViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+   private MutableLiveData<String> mText;
 
-    public DharmapalanViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
-    }
+   public DharmapalanViewModel() {
+      mText = new MutableLiveData<>();
+      mText.setValue("This is home fragment");
+   }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+   public LiveData<String> getText() {
+      return mText;
+   }
 }
